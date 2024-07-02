1eeceb2784e35ccaf15381c54622a41f5c7a1b2c
Shane Galvin <Sgalvin36@gmail.com>
2024-07-02 14:56:37 -0600

    Add dragon.rb

--- ruby_exercises/mythical-creatures/lib/dragon.rb
+++ ruby_exercises/mythical-creatures/lib/dragon.rb
@@ -1,11 +1,24 @@
-Class Dragon
+class Dragon
     attr_reader :name,
-                :gold,
+                :color,
                 :rider
 
-    def initialize(name, )
+    def initialize(name, color, rider)
         @name = name
-
+        @color = color
+        @rider = rider
+        @hungry = true
+        @hunger = 0
     end
 
+    def hungry?
+        @hungry
+    end
+
+    def eat
+        @hunger +=1
+        if @hunger == 3
+            @hungry = false
+        end
+    end
 end
