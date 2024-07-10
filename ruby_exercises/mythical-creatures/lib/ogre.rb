4d0d21c5d3fc40e68fcbed42fce62b7d99e88351
Shane Galvin <Sgalvin36@gmail.com>
2024-07-10 10:04:05 -0600

    Add human class and encounter logic between ogre and human

--- ruby_exercises/mythical-creatures/lib/ogre.rb
+++ ruby_exercises/mythical-creatures/lib/ogre.rb
@@ -2,8 +2,35 @@ class Ogre
     attr_reader :name,
                 :home
 
-    def initialize(name)
+    def initialize(name, home = "Swamp")
         @name = name
-        @home = "Swamp"
+        @home = home
+        @swings = 0
     end
+
+    def encounter(human)
+        human.encounter_counter += 1
+    end
+end
+
+class Human
+    attr_reader     :name
+
+    attr_accessor   :encounter_counter
+
+    def initialize(name = "Jane")
+        @name = name
+        @encounter_counter = 0
+        @notices_ogre = false
+    end
+
+    def notices_ogre?
+        if @encounter_counter < 3
+            @notices_ogre
+        else
+            @ecnounter_counter = 0
+            !@notices_ogre
+        end
+    end
+
 end
